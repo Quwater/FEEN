c15e0d075184e06b03cb6cf0ff95daed20773f22
Qcloud <qxwlxk@163.com>
2017-07-17 22:22:18 +0800

    [document]app.js: add functions

--- P5/js/app.js
+++ P5/js/app.js
@@ -1,4 +1,4 @@
-// 这是我们的玩家要躲避的敌人 
+// 这是我们的玩家要躲避的敌人
 var Enemy = function() {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
@@ -21,11 +21,24 @@ Enemy.prototype.render = function() {
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.handleInput = function() {
+
+};
+Player.prototype.checkCollisions = function() {
+
+};
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
+
+var allEnemies = [];
+var player = new Player();
 
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
